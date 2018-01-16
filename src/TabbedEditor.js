49f9052bb0804a4a369e30cd1c5354cb88d9a83e
Michael Ball <michaelaball@gmail.com>
2018-01-16 16:50:55 -0600

    Add buttons to editor that don't do anything yet

--- src/TabbedEditor.js
+++ src/TabbedEditor.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import 'react-tabs/style/react-tabs.css';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import {Tab, Tabs, TabList, TabPanel} from 'react-tabs';
 import Editor from "./Editor";
 
 class TabbedEditor extends Component {
@@ -8,31 +8,41 @@ class TabbedEditor extends Component {
         super(props);
     }
 
-    render () {
+    render() {
         var selectedIndex = this.props.tabbedEditor.openTabs.indexOf(
             this.props.tabbedEditor.openTabs.find((element) => element.id === this.props.tabbedEditor.activeTabID)
         );
         return (
-            <Tabs
-                selectedIndex={selectedIndex}
-                onSelect={tabIndex => this.props.switchEditorTab(this.props.tabbedEditor.openTabs[tabIndex])}>
-                <TabList>
+            <div align="left">
+                <div>
+                    <button>Info</button>
+                    <button>Deployments</button>
+                    <button>Run</button>
+                    <button>Save</button>
+                    <button>Revert</button>
+                    <button>Close</button>
+                </div>
+                <Tabs
+                    selectedIndex={selectedIndex}
+                    onSelect={tabIndex => this.props.switchEditorTab(this.props.tabbedEditor.openTabs[tabIndex])}>
+                    <TabList>
+                        {this.props.tabbedEditor.openTabs.map((script, index) => {
+                            return (<Tab>{script.name + " (" + script.id + ")"}</Tab>)
+                        })}
+                    </TabList>
                     {this.props.tabbedEditor.openTabs.map((script, index) => {
-                        return (<Tab>{script.name + " ("+script.id+")"}</Tab>)
+                        return (
+                            <TabPanel>
+                                <Editor
+                                    codemirrorhack={0}
+                                    script={script}
+                                    editorModification={this.props.editorModification}
+                                />
+                            </TabPanel>
+                        )
                     })}
-                </TabList>
-                {this.props.tabbedEditor.openTabs.map((script, index) => {
-                    return (
-                        <TabPanel>
-                            <Editor
-                                codemirrorhack={0}
-                                script={script}
-                                editorModification={this.props.editorModification}
-                            />
-                        </TabPanel>
-                    )
-                })}
-            </Tabs>
+                </Tabs>
+            </div>
         );
     }
 }
