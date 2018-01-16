17d642c3a7fe37cb17b6b7fb67a3f48cab885ac4
Michael Ball <michaelaball@gmail.com>
2018-01-16 13:59:46 -0600

    Tabs in editor with different dependency.

--- src/Editor.js
+++ src/Editor.js
@@ -1,9 +1,24 @@
 import React, {Component} from 'react';
+import 'react-tabs/style/react-tabs.css';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+
 class Editor extends Component {
     render () {
         //return null;
         return (
-            null
+            <Tabs>
+                <TabList>
+                    <Tab>Title 1</Tab>
+                    <Tab>Title 2</Tab>
+                </TabList>
+
+                <TabPanel>
+                    <h2>Any content 1</h2>
+                </TabPanel>
+                <TabPanel>
+                    <h2>Any content 2</h2>
+                </TabPanel>
+            </Tabs>
         );
     }
 }
