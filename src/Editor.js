e4922a0707af8acf10e05d14cfba155447b6d770
Michael Ball <michaelaball@gmail.com>
2018-01-16 13:50:33 -0600

    Remove tabs from editor for now.  They glitched.

--- src/Editor.js
+++ src/Editor.js
@@ -1,22 +1,9 @@
 import React, {Component} from 'react';
-import 'react-simpletabs/dist/react-simpletabs.css'
-import './tabs-custom.css'
-var Tabs = require('react-simpletabs');
-
 class Editor extends Component {
     render () {
+        //return null;
         return (
-            <Tabs>
-                <Tabs.Panel title='Tab #1'>
-                    <h2>Content #1 here</h2>
-                </Tabs.Panel>
-                <Tabs.Panel title='Tab #2'>
-                    <h2>Content #2 here</h2>
-                </Tabs.Panel>
-                <Tabs.Panel title='Tab #3'>
-                    <h2>Content #3 here</h2>
-                </Tabs.Panel>
-            </Tabs>
+            null
         );
     }
 }
