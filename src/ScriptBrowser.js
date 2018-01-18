e79b4177a823269a1c9fc48c05d8b5f2423dd81b
Michael Ball <michaelaball@gmail.com>
2018-01-18 03:55:08 -0600

    Are you sure you wish to delete the script?

--- src/ScriptBrowser.js
+++ src/ScriptBrowser.js
@@ -4,6 +4,8 @@ import checkboxHOC from 'react-table/lib/hoc/selectTable';
 import 'react-table/react-table.css'
 import ScriptDetail from "./ScriptDetail";
 import EditInfoModal from "./EditInfoModal";
+import {confirmAlert} from 'react-confirm-alert'; // Import
+import 'react-confirm-alert/src/react-confirm-alert.css' // Import css
 
 const ReactTable = require('react-table').default;
 const TableSelect = require('react-table-select');
@@ -45,7 +47,6 @@ class ScriptBrowser extends Component {
     }
 
 
-
     isSelected = (key) => {
         return this.props.scriptBrowser.selectedKey === key;
     };
@@ -54,7 +55,7 @@ class ScriptBrowser extends Component {
         this.props.updateScriptBrowser(
             Object.assign({}, this.props.scriptBrowser, {
                 selectedKey: key,
-                codemirrorhack: this.props.scriptBrowser.codemirrorhack+1,
+                codemirrorhack: this.props.scriptBrowser.codemirrorhack + 1,
             })
         );
     };
@@ -68,26 +69,39 @@ class ScriptBrowser extends Component {
 
     onClickEdit() {
         var script = this.props.scripts.find(script => script.id === this.props.scriptBrowser.selectedKey);
-        console.log("trying to open tab for: "+script);
+        console.log("trying to open tab for: " + script);
         this.props.openTabForScript(script);
     }
 
     onClickDelete() {
-        const deleteID = this.props.scriptBrowser.selectedKey;
-        superagent.delete(this.props.login.endpoint + scriptsEndpoint)
-            .set("api_key", this.props.login.token)
-            .query({id: deleteID})
-            .end((err, res) => {
-                if (err || res.statusCode !== 200) {
-                    return console.log(err);
-                }
-                console.log(res);
-                const data = this.props.scripts.filter(script => script.id !== deleteID);
-                this.props.updateScriptBrowser({
-                    selectedKey: null,
-                });
-                this.props.updateScripts(data);
-            });
+
+        confirmAlert({
+            title: 'Delete script?',                        // Title dialog
+            message: 'Are you sure you wish to delete the script? There is no way to retrieve it',               // Message dialog
+            childrenElement: () => {
+            },       // Custom UI or Component
+            confirmLabel: 'Yes, delete.',                           // Text button confirm
+            cancelLabel: 'No.',                             // Text button cancel
+            onConfirm: () => {
+                const deleteID = this.props.scriptBrowser.selectedKey;
+                superagent.delete(this.props.login.endpoint + scriptsEndpoint)
+                    .set("api_key", this.props.login.token)
+                    .query({id: deleteID})
+                    .end((err, res) => {
+                        if (err || res.statusCode !== 200) {
+                            return console.log(err);
+                        }
+                        console.log(res);
+                        const data = this.props.scripts.filter(script => script.id !== deleteID);
+                        this.props.updateScriptBrowser({
+                            selectedKey: null,
+                        });
+                        this.props.updateScripts(data);
+                    });
+            },    // Action after Confirm
+            onCancel: () => {
+            },      // Action after Cancel
+        });
     }
 
     onClickRefresh() {
@@ -102,7 +116,7 @@ class ScriptBrowser extends Component {
             toggleAll,
             selectType: 'radio',
         };
-        const script=this.props.scripts.find((element) => {
+        const script = this.props.scripts.find((element) => {
             return element.id === this.props.scriptBrowser.selectedKey
         });
         const scriptDetail = (script === undefined) ? null :
@@ -110,7 +124,7 @@ class ScriptBrowser extends Component {
                 script={script}
                 codemirrorhack={this.props.scriptBrowser.codemirrorhack}
             />;
-        console.log("script: "+script);
+        console.log("script: " + script);
 
         return (
             <div style={{height: '100%'}}>
@@ -137,8 +151,12 @@ class ScriptBrowser extends Component {
                     <div style={{float: 'left', width: '60%', marginBottom: '60px'}}>
                         <div align="left">
                             <button onClick={this.onClickNew}>New</button>
-                            <button onClick={this.onClickEdit} disabled={this.props.scriptBrowser.selectedKey===null ? "disabled" : null}>Edit</button>
-                            <button onClick={this.onClickDelete} disabled={this.props.scriptBrowser.selectedKey===null ? "disabled" : null}>Delete</button>
+                            <button onClick={this.onClickEdit}
+                                    disabled={this.props.scriptBrowser.selectedKey === null ? "disabled" : null}>Edit
+                            </button>
+                            <button onClick={this.onClickDelete}
+                                    disabled={this.props.scriptBrowser.selectedKey === null ? "disabled" : null}>Delete
+                            </button>
                             <button onClick={this.onClickRefresh}>Refresh</button>
                         </div>
                         <CheckboxTable
